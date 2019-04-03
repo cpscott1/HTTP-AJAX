0314bf2c5b9f1dcd0b77e371c3f329197cbe1822
cpscott1 <thacam18@gmail.com>
2019-04-02 20:32:38 -0500

    adding data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import FriendsList from './Friend/FriendsList';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Header</h1>
-        </header>
+        <FriendsList />
       </div>
     );
   }
--- friends/src/Friend/FriendsList.js
+++ friends/src/Friend/FriendsList.js
@@ -1,2 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+
+export default class FriendsList extends Component {
+  state = {
+    friends: [],
+    error: ''
+  }
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/friends')
+    .then(res => {
+      console.log(res)
+    })
+  }
+
+  render() {
+    return (
+      <div>FriendsList</div>
+    )
+  }
+
+
+}
