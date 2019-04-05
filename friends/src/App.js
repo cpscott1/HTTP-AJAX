87ed97a11c832898509658195ca6179a09b8524a
cpscott1 <thacam18@gmail.com>
2019-04-04 22:42:14 -0500

    added change handler

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import FriendsList from './Friend/FriendsList';
 import './App.css';
 
 class App extends Component {
+  state = {
+    friends: [],
+    newFriend: {
+      name: '',
+      age: '',
+      email: ''
+    }
+  }
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/friends')
+    .then(res => {
+      console.log(res)
+      this.setState( () => ({ friends: res.data }))
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+handleChanges = event => {
+  this.setState(prevState => {
+      return {
+        newFriend: {
+        ...prevState.newFriend,
+        [event.target.name]: event.target.value
+      }
+    }
+  })
+}
+
   render() {
     return (
       <div className="App">
